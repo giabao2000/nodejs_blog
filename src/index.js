310075a882f98e5a5ed633fd3348867c02d945f3
giabao2000 <dinhgiabao0506@gmail.com>
2021-08-30 09:50:41 +0700

    Install prettier, lint-staged, husky

--- src/index.js
+++ src/index.js
@@ -3,21 +3,35 @@ const express = require("express");
 const morgan = require("morgan");
 const handlebars = require("express-handlebars");
 
+const route = require("./routes");
+
 const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// MiddleWare
+// 1. Xử lý data từ form
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+
+// 2. Xử lý data bằng JS hoặc html
+// XMLHttpRequest, fetch, axios, ...
+app.use(express.json());
+
 // HTTP logger
-app.use(morgan("combined"));
+// app.use(morgan("combined"));
 
 // Template engine
 // Config đuôi file handlebars => hbs (cho gọn)
 app.engine(
-  "hbs",
-  handlebars({
-    extname: ".hbs",
-  })
+    "hbs",
+    handlebars({
+        extname: ".hbs",
+    })
 );
 
 app.set("view engine", "hbs");
@@ -25,14 +39,16 @@ app.set("view engine", "hbs");
 // Cài đặt lại đường dẫn thư mục cho thư mục views
 app.set("views", path.join(__dirname, "resources\\views"));
 
-console.log("PATH: ", path.join(__dirname, "resources\\views"));
+// console.log("PATH: ", path.join(__dirname, "resources\\views"));
 
-// Router
-app.get("/", (req, res) => res.render("home"));
+// Router init
+route(app);
 
-// News
-app.get("/news", (req, res) => res.render("news"));
+// app.post("/search", (req, res) => {
+//   console.log(req.body);
+//   res.send("");
+// });
 
 app.listen(port, () =>
-  console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:${port}`)
 );
